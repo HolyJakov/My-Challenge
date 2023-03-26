5d03c75f71b857369bfe5f912450d928406b6bae
Holy Ojakovo <ojakovoofuafo@gmail.com>
2023-03-26 23:32:04 +0100

    work

---
+++ first-project/index.js
@@ -0,0 +1,14 @@
+// const dog = document.getElementById("dog");
+// const fish = document.getElementById("fish");
+// const lion = document.getElementById("lion");
+// const parrot = document.getElementById("parrot");
+// const turtle = document.getElementById("turtle");
+
+// let count =0;
+
+// const animals = ["dog","fish","lion","parrot","turtle"];
+// function display(){
+// for (let i =0; i < animals.length; i++){
+
+//     }
+// }
